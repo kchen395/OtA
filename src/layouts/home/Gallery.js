efa95f4aece3bc27ebf17a7aa8fac4ff315fed66
Work <work@casey395-126090.home>
2018-11-21 12:59:24 -0500

    Implement donate feature for app

--- src/layouts/home/Gallery.js
+++ src/layouts/home/Gallery.js
@@ -5,66 +5,73 @@ export default class Gallery extends Component {
     super(props);
     this.state = {
       amount: null
-		};
-		this.onChange = this.onChange.bind(this);
-	}
-	
-	onChange(e) {
-		this.setState({amount: e.target.value})
-	}
+    };
+    this.onChange = this.onChange.bind(this);
+  }
 
-	render() {
-		const {data, like, donate} = this.props
-		return (
-			<table>
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Work</th>
-						<th>Description</th>
-						<th>Upvotes</th>
-						<th>Support Artist</th>
-					</tr>
-				</thead>
-				<tbody>
-					{data
-						.filter(item => item.name)
-						.map((item, i) => {
-							return (
-								<tr key={i}>
-									<td>{item.name}</td>
-									<td>
-										<a href={item.link}>
-											<img
-												src={item.thumbnail}
-												alt={"Image " + i}
-												display="block"
-												width="200px"
-												height="auto"
-											/>
-										</a>
-									</td>
-									<td>{item.description}</td>
-									<td>
-										<button
-											className="btn"
-											onClick={() => like(item.id)}
-										>
-											<i className="fa fa-caret-up" />
-										</button>
-										<div>{item.upvotes}</div>
-									</td>
-									<td>
-										<form>
-											<input type="number" min="0" step="0.01" onChange={this.onChange}></input>
-										</form>
-										<button className="pure-button" onClick={() => donate(item.address, this.state.amount)}>Send Ether</button>
-									</td>
-								</tr>
-							);
-						})}
-				</tbody>
-			</table>
-		);	
-	}
-};
+  onChange(e) {
+    this.setState({ amount: e.target.value });
+  }
+
+  render() {
+    const { data, like, donate } = this.props;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Work</th>
+            <th>Description</th>
+            <th>Upvotes</th>
+            <th>Support Artist</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data
+            .filter(item => item.name)
+            .map((item, i) => {
+              return (
+                <tr key={i}>
+                  <td>{item.name}</td>
+                  <td>
+                    <a href={item.link}>
+                      <img
+                        src={item.thumbnail}
+                        alt={"Image " + i}
+                        display="block"
+                        width="200px"
+                        height="auto"
+                      />
+                    </a>
+                  </td>
+                  <td>{item.description}</td>
+                  <td>
+                    <button className="btn" onClick={() => like(item.id)}>
+                      <i className="fa fa-caret-up" />
+                    </button>
+                    <div>{item.upvotes}</div>
+                  </td>
+                  <td>
+                    <form>
+                      <input
+                        type="number"
+                        min="0"
+                        step="0.01"
+                        onChange={this.onChange}
+                      />
+                    </form>
+                    <button
+                      className="pure-button"
+                      onClick={() => donate(item.address, this.state.amount)}
+                    >
+                      Send Ether
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    );
+  }
+}
